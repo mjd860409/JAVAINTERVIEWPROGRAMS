0ce005f9d00105da61ea761b10fec066a0421f60
manas <mjdas731@gmail.com>
2023-11-11 15:25:35 +0530

    second commit

--- Array/OccurrenceOfEachElementInAnArray.java
+++ Array/OccurrenceOfEachElementInAnArray.java
@@ -1,27 +1,21 @@
 public class OccurrenceOfEachElementInAnArray {
     public static void main(String[] args) {
         int arr[] = {1,2,3,1,2,4,5,3};
-        int temp[] = new int[arr.length];
-        int visited = -1;
-
-        for(int i = 0; i<arr.length; i++){
-            int count = 1;
-            for(int j = i+1; j<arr.length; j++){
-                if (arr[i]==arr[j]) {
-                    count++;
-                    temp[j] = visited; 
+        
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = i  ; j < arr.length; j++) {
+                if (arr[i] == arr[j] && i>j) {
+                    break;
                 }
-            }
-        
-            if (temp[i]!=visited) {
-                temp[i] = count;
-            }
-        
-        }
-    
-        for(int i = 0; i<temp.length; i++){
-            if (temp[i]!=visited) {
-                System.out.println("Frequence of elements : " + arr[i] + "::" + temp[i]);
+            
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            
+                if (j == arr.length-1) {
+                    System.out.print(arr[i] + " " + count);
+                }
             }
         }
     }
